c56abfb0a1c171b729a3994f2850257f8806654d
tnaswin <tnaswin007@gmail.com>
2020-05-15 17:24:32 +0530

    Update/Delete planet using PUT/DEL method

--- app.py
+++ app.py
@@ -197,6 +197,36 @@ def add_planet():
         return jsonify(message="You added a planet."), 201
 
 
+@app.route('/update_planet', methods=['PUT'])
+@jwt_required
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message="You updated a planet"), 202
+    else:
+        return jsonify(message="That planet does not exist."), 404
+
+
+@app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
+@jwt_required
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message="You deleted a planet"), 202
+    else:
+        return jsonify(message="That planet does not exist"), 404
+
+
 # database models
 class User(db.Model):
     __tablename__ = 'users'
